50e1a6a38814d2eca77e89b3c888238c59b0fe65
jspc <james.condron@beamly.com>
2017-09-11 13:47:36 +0100

    Make network requests

--- url.go
+++ url.go
@@ -2,7 +2,20 @@ package gusha
 
 import (
 	"fmt"
+	"net/http"
+	"net/url"
 	"strings"
+
+	"github.com/satori/go.uuid"
+)
+
+type httpClient interface {
+	Do(*http.Request) (*http.Response, error)
+}
+
+var (
+	BaseURL            = "example.com"
+	Client  httpClient = &http.Client{}
 )
 
 // URLs is a shorthand type for a slice of URL types
@@ -22,14 +35,33 @@ type URL struct {
 // String returns a textual representation of a URL
 // suitable for logging
 func (u URL) String() string {
+	return fmt.Sprintf("%s %s", u.Method, u.urlString("", "true"))
+}
+
+// Do wraps net/http Client.Do and shadows its return types.
+// It exists solely to allow for us to manipulate URLs at runtime
+// where Cache Bust behaviour is required
+func (u URL) Do() (r *http.Response, err error) {
+	pU, err := url.Parse(u.urlString(BaseURL, uuid.NewV4().String()))
+	if err != nil {
+		return
+	}
+
+	return Client.Do(&http.Request{
+		Method: u.Method,
+		URL:    pU,
+	})
+}
+
+func (u URL) urlString(base, cacheBuster string) string {
 	params := []string{}
 	for k, v := range u.Params {
 		params = append(params, fmt.Sprintf("%s=%v", k, v))
 	}
 
 	if u.CacheBust {
-		params = append(params, "cache_bust=true")
+		params = append(params, fmt.Sprintf("cache_bust=%s", cacheBuster))
 	}
 
-	return fmt.Sprintf("%s %s?%s", u.Method, u.Path, strings.Join(params, "&"))
+	return fmt.Sprintf("%s%s?%s", base, u.Path, strings.Join(params, "&"))
 }
--- url_test.go
+++ url_test.go
@@ -2,7 +2,23 @@ package gusha
 
 import (
 	"fmt"
+	"net/http"
+	"testing"
 )
+
+var (
+	emptyParams  = map[string]interface{}{}
+	fooBarParams = map[string]interface{}{"foo": "bar"}
+)
+
+type testHTTPClient struct {
+	url string
+}
+
+func (thc *testHTTPClient) Do(r *http.Request) (*http.Response, error) {
+	thc.url = r.URL.String()
+	return &http.Response{}, nil
+}
 
 func ExampleURL() {
 	url := URL{
@@ -14,4 +30,71 @@ func ExampleURL() {
 
 	fmt.Println(url.String())
 	// Output: GET /?token={{ .APIKey }}&cache_bust=true
+}
+
+func TestURL(t *testing.T) {
+	for _, domain := range []string{"", "https://example.com", "https://maxfactor.co.uk"} {
+
+		for _, test := range []struct {
+			name         string
+			path         string
+			params       map[string]interface{}
+			cacheBust    bool
+			expectString string
+		}{
+			{fmt.Sprintf("Request on domain %q, with no params, no cachebust", domain), "/", emptyParams, false, "/?"},
+			{fmt.Sprintf("Request on domain %q, with no params, with cachebust", domain), "/", emptyParams, true, "/?cache_bust=true"},
+			{fmt.Sprintf("Request on domain %q, with params, no cachebust", domain), "/", fooBarParams, false, "/?foo=bar"},
+			{fmt.Sprintf("Request on domain %q, with params, with cachebust", domain), "/", fooBarParams, true, "/?foo=bar&cache_bust=true"},
+		} {
+			t.Run(test.name, func(t *testing.T) {
+				u := URL{
+					Method:    "GET",
+					Path:      test.path,
+					Params:    test.params,
+					CacheBust: test.cacheBust,
+				}
+
+				uString := u.urlString(domain, "true")
+				fqExpectString := fmt.Sprintf("%s%s", domain, test.expectString)
+
+				if fqExpectString != uString {
+					fmt.Errorf("expected %q, received %q", fqExpectString, uString)
+				}
+			})
+		}
+
+	}
+}
+
+func TestDo(t *testing.T) {
+	for _, test := range []struct {
+		name      string
+		domain    string
+		path      string
+		params    map[string]interface{}
+		cacheBust bool
+	}{
+		{"Request with no params, no cachebust", "coty.com", "/about-us", emptyParams, false},
+		{"Request with no params, with cachebust", "coty.com", "/about-us", emptyParams, true},
+		{"Request with params, no cachebust", "coty.com", "/about-us", fooBarParams, false},
+		{"Request with params, with cachebust", "coty.com", "/about-us", fooBarParams, true},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			u := URL{
+				Method:    "GET",
+				Path:      test.path,
+				Params:    test.params,
+				CacheBust: test.cacheBust,
+			}
+
+			BaseURL = test.domain
+			Client = &testHTTPClient{}
+
+			_, err := u.Do()
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+		})
+	}
 }
